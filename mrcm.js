376157bf342c44b64270f70f21793c8258d86458
Tara Roys <tlroys@gmail.com>
2012-05-09 11:33:17 -0500

    Revert back to Serpinski's Gasket Demo. I forget what the changes made between the original commit and this commit are.  Demo works only on ipad.

--- mrcm.js
+++ mrcm.js
@@ -1,61 +1,107 @@
 window.onload=function(){
 	
-	var drawingCanvas = function(canvas_object){
-
-		var drawer = {
-			touchstart:function(coords, draw_context){ 
-				draw_context.beginPath();
-				draw_context.moveTo(coords.x, coords.y);
-			},
-			touchmove:function(coords, draw_context){
-					draw_context.lineTo(coords.x, coords.y);
-					draw_context.stroke();
-			},
-			touchend:function(coords, draw_context){
-					this.touchmove(coords);
-					
+	
+	
+	
+	var canvas=document.getElementById("canvas");
+	var ctx=canvas.getContext("2d");
+	
+	
+	var buffer = document.createElement('canvas');
+    buffer.width = canvas.width;
+    buffer.height = canvas.height;
+    var bufctx = buffer.getContext('2d');
+	
+	var drawer = {
+		isDrawing:false,
+		touchstart:function(coords){ 
+			this.isDrawing=true;
+			ctx.beginPath();
+			ctx.moveTo(coords.x, coords.y);
+		},
+		touchmove:function(coords){
+			if(this.isDrawing){
+				ctx.lineTo(coords.x, coords.y);
+				ctx.stroke();
 			}
-		};
-		
-		var draw = function(event){
-			var ctx=this.getContext("2d");
-			ctx.lineWidth=10;
-			var coords= { 
-				x: event.changedTouches[0].pageX,
-				y: event.changedTouches[0].pageY
-			};
-			drawer[event.type](coords, ctx);
-		};
-		
-		canvas_object.addEventListener("touchstart", draw, false);
-		canvas_object.addEventListener("touchmove", draw, false);
-		canvas_object.addEventListener("touchend", draw, false);
-	}
-	
-	drawingCanvas(document.getElementById("canvas"));
-	
-	var copyCanvas = function(canvas_to_be_copied){
-		
-		var copy=function(event){
-			var canvas_copy = document.createElement('canvas');
-    		canvas_copy.width = canvas_to_be_copied.width;
-    		canvas_copy.height = canvas_to_be_copied.height;
-    		var canvas_copy_ctx = canvas_copy.getContext('2d');
-    		
-    		var ctx=canvas_to_be_copied.getContext('2d');
-    		
-    		canvas_copy_ctx.drawImage(canvas_to_be_copied, 0, 0);
-    		
-    		
-    		
-    		document.body.appendChild(canvas_copy);
+		},
+		touchend:function(coords){
+			if(this.isDrawing)
+			{
+				this.touchmove(coords);
+				this.isDrawing=false;
+			}
+			
 		}
-		copy_button.addEventListener("click", copy, false );
-	}
 	
-	copyCanvas(document.getElementById("canvas"));
+	};
+	
+	var draw = function(event){
+		
+		var coords= { 
+		x: event.changedTouches[0].pageX,
+		y: event.changedTouches[0].pageY
+		
+		};
+		drawer[event.type](coords);
+	};
+	canvas.addEventListener("touchstart", draw, false);
+	canvas.addEventListener("touchmove", draw, false);
+	canvas.addEventListener("touchend", draw, false);
+	
 	
 	document.body.addEventListener('touchmove',function(event){
 			event.preventDefault();
 		},false);
+	
+	var makeIntoGasket = function(){
+	
+		bufctx.clearRect(0,0, buffer.width, buffer.height);
+    	bufctx.drawImage(canvas, 0, 0);
+		ctx.clearRect(0,0, canvas.width, canvas.height); //clears canvas as well as saving it. 
+		
+		var width=canvas.width;
+		var height=canvas.height;
+			
+		ctx.save();
+		ctx.transform(0.5, 0, 0, 0.5, 0, 100);
+		ctx.drawImage(buffer,0,0);
+		//ctx.drawImage(image, 0, 0);
+		ctx.restore();
+		
+		//draw copy 2
+		
+		ctx.save();
+		ctx.transform(0.5, 0, 0, 0.5, 100, 0);
+		ctx.drawImage(buffer,0,0);
+		//ctx.drawImage(image, 0, 0);
+		ctx.restore();
+		
+		//draw copy 3
+		ctx.save();
+		ctx.transform(0.5, 0, 0, 0.5, 0, 0);
+		ctx.drawImage(buffer,0,0);
+		//ctx.drawImage(image, 0, 0);
+		ctx.restore();
+		
+		//make pixels darker on each iteration
+		var original = ctx.getImageData( 0, 0, width, height);
+		
+		var pixels = width*height*4;
+	
+		while(pixels--){
+			if(original.data[pixels] !== 0){
+				original.data[pixels]=255;
+			}
+		}
+		
+		ctx.putImageData(original, 0, 0);
+		
+	}
+	
+	
+	var  make_into_gasket=document.getElementById("make_into_gasket");
+	make_into_gasket.addEventListener("click", makeIntoGasket, false);
+	
+	
 }
