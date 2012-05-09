514061dca925688c7adaba6c0e5e3c7190e662db
Tara Roys <tlroys@gmail.com>
2012-05-09 11:53:05 -0500

    Changed the id of the button.  Changed the transform values.  Rewrote the script so that it was divided into two functions:  drawingCanvas, which controls the drawing on the canvas functionality, and multipleReductionCopyMachine, which controls the turning the drawing into a fractal functionality.  Each function takes in a canvas as a parameter and draws the result to the canvas on the page.  (Not quite functional, but as close as I could get while using javascript events.  The only side effect each function is allowed to have is drawing something on the canvas.) Also implemented new coding style:  Whereever possible, the variables used inside a function are passed in as parameters or declared within the function.  This is an attempt to keep sphagetti code at a minimum. Still only works on Ipad.

--- mrcm.js
+++ mrcm.js
@@ -1,107 +1,89 @@
 window.onload=function(){
-	
-	
-	
-	
-	var canvas=document.getElementById("canvas");
-	var ctx=canvas.getContext("2d");
-	
-	
-	var buffer = document.createElement('canvas');
-    buffer.width = canvas.width;
-    buffer.height = canvas.height;
-    var bufctx = buffer.getContext('2d');
-	
-	var drawer = {
-		isDrawing:false,
-		touchstart:function(coords){ 
-			this.isDrawing=true;
-			ctx.beginPath();
-			ctx.moveTo(coords.x, coords.y);
-		},
-		touchmove:function(coords){
-			if(this.isDrawing){
-				ctx.lineTo(coords.x, coords.y);
-				ctx.stroke();
+
+	var drawingCanvas = function(canvas){
+		var drawer = {
+			touchstart:function(coords, draw_context){ 
+				draw_context.beginPath();
+				draw_context.moveTo(coords.x, coords.y);
+			},
+			touchmove:function(coords, draw_context){
+					draw_context.lineTo(coords.x, coords.y);
+					draw_context.stroke();
+			},
+			touchend:function(coords, draw_context){
+					this.touchmove(coords);
 			}
-		},
-		touchend:function(coords){
-			if(this.isDrawing)
-			{
-				this.touchmove(coords);
-				this.isDrawing=false;
-			}
-			
-		}
-	
-	};
-	
-	var draw = function(event){
-		
-		var coords= { 
-		x: event.changedTouches[0].pageX,
-		y: event.changedTouches[0].pageY
-		
 		};
-		drawer[event.type](coords);
+		
+		var draw = function(event){
+			var ctx=this.getContext("2d");
+			ctx.lineWidth=10;
+			var coords= { 
+				x: event.changedTouches[0].pageX,
+				y: event.changedTouches[0].pageY
+			};
+			drawer[event.type](coords, ctx);
+		}; 
+		canvas.addEventListener("touchstart", draw, false);
+		canvas.addEventListener("touchmove", draw, false);
+		canvas.addEventListener("touchend", draw, false);
 	};
-	canvas.addEventListener("touchstart", draw, false);
-	canvas.addEventListener("touchmove", draw, false);
-	canvas.addEventListener("touchend", draw, false);
 	
+	drawingCanvas(document.getElementById("canvas"));
 	
+	var multipleReductionCopyMachine = function(canvas){
+		
+		var copy=function(event){
+			//create a copy of the original image.  
+			
+			var buffer = document.createElement('canvas');
+    		buffer.width = canvas.width;
+    		buffer.height = canvas.height;
+    		var bufctx = buffer.getContext('2d');
+    		bufctx.drawImage(canvas, 0, 0);
+    		
+    		var ctx=canvas.getContext('2d');
+    		ctx.clearRect(0,0, canvas.width, canvas.height)
+    		
+	    		ctx.save();
+				ctx.transform(0.5, 0, 0, 0.5, canvas.width/2, canvas.width/2);
+				ctx.drawImage(buffer,0,0);
+				//ctx.drawImage(image, 0, 0);
+				ctx.restore();
+			
+				//draw copy 2
+			
+				ctx.save();
+				ctx.transform(0.5, 0, 0, 0.5, canvas.width/4, 0);
+				ctx.drawImage(buffer,0,0);
+				//ctx.drawImage(image, 0, 0);
+				ctx.restore();
+				
+			
+				//draw copy 3
+				ctx.save();
+				ctx.transform(0.5, 0, 0, 0.5, 0, canvas.width/2);
+				ctx.drawImage(buffer,0,0)
+				//ctx.drawImage(image, 0, 0);
+				ctx.restore();
+			
+			var original = ctx.getImageData( 0, 0, canvas.width, canvas.height);
+			
+			var pixels = canvas.width*canvas.height*4;
+	
+			while(pixels--){
+				if(original.data[pixels] !== 0){
+					original.data[pixels]=255;
+				}
+			}
+			ctx.putImageData(original, 0, 0);
+		}
+		
+		copy_button.addEventListener("click", copy, false );
+	}
+	
+	multipleReductionCopyMachine(document.getElementById("canvas"));
 	document.body.addEventListener('touchmove',function(event){
 			event.preventDefault();
 		},false);
-	
-	var makeIntoGasket = function(){
-	
-		bufctx.clearRect(0,0, buffer.width, buffer.height);
-    	bufctx.drawImage(canvas, 0, 0);
-		ctx.clearRect(0,0, canvas.width, canvas.height); //clears canvas as well as saving it. 
-		
-		var width=canvas.width;
-		var height=canvas.height;
-			
-		ctx.save();
-		ctx.transform(0.5, 0, 0, 0.5, 0, 100);
-		ctx.drawImage(buffer,0,0);
-		//ctx.drawImage(image, 0, 0);
-		ctx.restore();
-		
-		//draw copy 2
-		
-		ctx.save();
-		ctx.transform(0.5, 0, 0, 0.5, 100, 0);
-		ctx.drawImage(buffer,0,0);
-		//ctx.drawImage(image, 0, 0);
-		ctx.restore();
-		
-		//draw copy 3
-		ctx.save();
-		ctx.transform(0.5, 0, 0, 0.5, 0, 0);
-		ctx.drawImage(buffer,0,0);
-		//ctx.drawImage(image, 0, 0);
-		ctx.restore();
-		
-		//make pixels darker on each iteration
-		var original = ctx.getImageData( 0, 0, width, height);
-		
-		var pixels = width*height*4;
-	
-		while(pixels--){
-			if(original.data[pixels] !== 0){
-				original.data[pixels]=255;
-			}
-		}
-		
-		ctx.putImageData(original, 0, 0);
-		
-	}
-	
-	
-	var  make_into_gasket=document.getElementById("make_into_gasket");
-	make_into_gasket.addEventListener("click", makeIntoGasket, false);
-	
-	
 }
