8b3c4b604c5b44483db1e1822a507487fbc5e510
Tara Roys <tlroys@gmail.com>
2012-05-09 13:42:04 -0500

    Changed animation to use setInterval instead.  This is a reduced version that only copies one transform in order to get used to using setInterval.  Set Interval works in the following manner:  it calls a function once every set interval of time.  For my purposes, it draws the next frame every set interval of time. Hmm.  I ought to rename the animate fuction drawNextFrame, because that in all honesty is what it does.  After a set amount of frames, I have to call clearInterval to stop the animation.

--- mrcm.js
+++ mrcm.js
@@ -1,7 +1,6 @@
 window.onload=function(){
 
 	var findPos = function(obj){
-		
 	
 			current_pos_left=current_pos_top=0;
 			if(obj.offsetParent){
@@ -59,7 +58,6 @@ window.onload=function(){
     		var bufctx = buffer.getContext('2d');
     		bufctx.drawImage(canvas, 0, 0);
     		
-    		
     		var animation_buffer = document.createElement('canvas');
     		animation_buffer.width = canvas.width;
     		animation_buffer.height = canvas.height;
@@ -70,24 +68,18 @@ window.onload=function(){
     		copy1.height = canvas.height;
     		var copy1ctx = copy1.getContext('2d');
     		
-    		var copy2 = document.createElement('canvas');
-    		copy2.width = canvas.width;
-    		copy2.height = canvas.height;
-    		var copy2ctx = copy2.getContext('2d');
-    		
-    		var copy3 = document.createElement('canvas');
-    		copy3.width = canvas.width;
-    		copy3.height = canvas.height;
-    		var copy3ctx = copy3.getContext('2d');
-    		
-    		
     		var ctx=canvas.getContext('2d');
     		ctx.clearRect(0,0, canvas.width, canvas.height);
     		
-    		var i=0;
+    		var transformCanvas = function(canvas, xform_object){
     		
+    		};
+    		
+    		var i=0;
     			var animate = function() {
     				ctx.clearRect(0,0, canvas.width, canvas.height);
+    				
+    				//replace this with a function that takes in a canvas and a transform matrix and outputs a canvas.  
     				
 						anibufctx.clearRect(0,0, canvas.width, canvas.height);
 						
@@ -98,91 +90,11 @@ window.onload=function(){
 					
 					ctx.drawImage(animation_buffer,0,0);
 					i++;
-					if (i !==6){
-					setTimeout(animate, 100);
-					}
-					else{
+					
+					if (i === 6){
+					clearInterval(id);
 					copy1ctx.drawImage(animation_buffer,0,0);
-					setTimeout(animate2, 100);
 					}
-					
-				
-					var original = ctx.getImageData( 0, 0, canvas.width, canvas.height);
-			
-					var pixels = canvas.width*canvas.height*4;
-	
-					while(pixels--){
-						if(original.data[pixels] !== 0){
-							original.data[pixels]=255;
-						}
-					}
-					ctx.putImageData(original, 0, 0);
-	    			
-	    			};
-	    			
-	    			var j = 0;
-	    			var animate2 = function() {
-    				ctx.clearRect(0,0, canvas.width, canvas.height);
-    				ctx.drawImage(copy1,0,0);
-    				
-						anibufctx.clearRect(0,0, canvas.width, canvas.height);
-						
-						//draw copy 1
-						anibufctx.save();
-						anibufctx.transform((1-j/10), 0, 0, (1-j/10), j/5*canvas.width/4, 0);
-						anibufctx.drawImage(buffer,0,0);
-						anibufctx.restore();
-					
-					ctx.drawImage(animation_buffer,0,0);
-					j++;
-					if (j !==6){
-					
-					setTimeout(animate2, 100);
-					}
-					else{
-					copy2ctx.drawImage(animation_buffer,0,0);
-					setTimeout(animate3, 100);
-					}
-					
-				
-					var original = ctx.getImageData( 0, 0, canvas.width, canvas.height);
-			
-					var pixels = canvas.width*canvas.height*4;
-	
-					while(pixels--){
-						if(original.data[pixels] !== 0){
-							original.data[pixels]=255;
-						}
-					}
-					ctx.putImageData(original, 0, 0);
-	    			
-	    			};
-	    			
-	    			
-	    			var k = 0;
-	    			var animate3 = function() {
-    				ctx.clearRect(0,0, canvas.width, canvas.height);
-    				ctx.drawImage(copy1,0,0);
-    				ctx.drawImage(copy2,0,0);
-						anibufctx.clearRect(0,0, canvas.width, canvas.height);
-						
-						
-						
-						//draw copy 3
-						anibufctx.save();
-						anibufctx.transform((1-k/10), 0, 0, (1-k/10), 0, k/5*canvas.width/2);
-						anibufctx.drawImage(buffer,0,0)
-						anibufctx.restore();
-						
-					
-					
-					ctx.drawImage(animation_buffer,0,0);
-					k++;
-					if (k !==6){
-					
-					setTimeout(animate3, 100);
-					}
-					
 				
 					var original = ctx.getImageData( 0, 0, canvas.width, canvas.height);
 			
@@ -197,7 +109,7 @@ window.onload=function(){
 	    			
 	    			};
     		
-					setTimeout(animate, 100);
+					var id = setInterval(animate, 100);
 		}
 		
 		copy_button.addEventListener("click", copy, false );
